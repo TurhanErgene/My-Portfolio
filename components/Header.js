572bbd4d6b213148a3c94c35b3a6287b798ede64
Turhan <turhan-ergene@hotmail.com>
2021-12-29 23:28:22 +0100

    Theme

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between text-white px-6 py-4 h-16  max-w-5xl mx-auto mt-12 bg-transparent">
       <span></span>
-      <button className="px-6 py-2 bg-pink-700 text-white rounded-md font-semibold uppercase">Dark</button>
+      <button className="px-6 py-2 bg-pink-700 text-white rounded-md font-semibold uppercase" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>{theme === "light" ? "Dark" : "Light"}</button>
     </header>
   )
 }
