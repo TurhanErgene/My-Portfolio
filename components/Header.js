6da9e3b429706dadba0538c369383fef07d18280
Turhan <turhan-ergene@hotmail.com>
2021-12-30 23:06:24 +0100

    header

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import { useTheme } from 'next-themes'
+import React from "react";
+import { useTheme } from "next-themes";
 
 const Header = () => {
+  const { theme, setTheme } = useTheme();
+
   return (
     <header className="flex items-center justify-between text-white px-6 py-4 h-16  max-w-5xl mx-auto mt-12 bg-transparent">
       <span></span>
-      <button className="px-6 py-2 bg-pink-700 text-white rounded-md font-semibold uppercase" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>{theme === "light" ? "Dark" : "Light"}</button>
+      <button
+        className="px-6 py-2 bg-pink-700 text-white rounded-md font-semibold uppercase"
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      >
+        {theme === "light" ? "Dark" : "Light"}
+      </button>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
       
       
       
-      <main>
-        <h1 className="text-green-500">Hello World</h1>
+      <main className="w-full h-full">
+        <h1 className="text-white-500 dark:text-gray-800">Hello World</h1>
       </main>
     </Layout>
     
